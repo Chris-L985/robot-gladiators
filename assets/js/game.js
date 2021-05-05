52796992b0fb5765d650c88be568f55411e2980f
Chris-L985 <thelopezdispenser@gmail.com>
2021-05-05 01:12:40 -0600

    added player money to the site

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
-var PlayerMoney = 10;
+var playerMoney = 10;
 
 // you can also log multiple values at once like this console.log(playerName, playerAttack, playerHealth);
 
@@ -13,16 +13,14 @@ var enemyAttack = 12;
 // create fight function
 var fight = function(enemyName) {
     // alert players that they are starting the round
-  while(enemyHealth > 0) { 
+  while(playerHealth > 0 && enemyHealth > 0) { 
   
     //fight prompt
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     //if player choses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
-        //remove enemy's health by subtracting the amount set in the playerAttack variable
-        // if player chooses to skip
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
+    
+    if (promptFight === "skip" || promptFight === "SKIP") {
       // confirm player wants to skip
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -31,6 +29,7 @@ var fight = function(enemyName) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
         playerMoney = playerMoney - 10;
+        console.log("playerMoney", playerMoney);
         break;
       }
     }
@@ -44,6 +43,11 @@ var fight = function(enemyName) {
       // check enemy's health
       if (enemyHealth <= 0) {
         window.alert(enemyName + " has died! ");
+
+        // award player money for winning
+        playerMoney = playerMoney + 20;
+
+        break;
       } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
       }
@@ -57,17 +61,18 @@ var fight = function(enemyName) {
       // check player's health
       if (playerHealth <= 0) {
         window.alert(playerName + " has died! ");
+        break;
       } else {
         window.alert(playerName + " still has " + playerHealth + " health left. ");
       }
   }
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
-  debugger;
+//for (var i = 0; i < enemyNames.length; i++) {
+  //debugger;
   // call fight function with enemy-robot
-  fight(enemyNames[i]);
-}
+  //fight(enemyNames[i]);
+//}
 // execute fight
 for(var i = 0; i < enemyNames.length; i++) {
   var pickedEnemyName = enemyNames[i];
